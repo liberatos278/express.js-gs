5c433bc166d9f30a544ed61c1fca7bb74d7bc1bb
Liberatos <ja@nectu.email>
2023-08-08 19:25:45 +0200

    mockup data eng

--- eng/controller/grade.controller.js
+++ eng/controller/grade.controller.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const router = express.Router()
 
+const grades = []
+
 router.get("/list", (req, res) => {
-  const grades = []
   res.json(grades)
 })
 
--- eng/controller/student.controller.js
+++ eng/controller/student.controller.js
@@ -1,13 +1,13 @@
 const express = require("express")
 const router = express.Router()
 
-router.get("/list", (req, res) => {
-  const students = [
-    { id: 1, firstname: "Petr", surname: "Novák", nationalId: "123456789" },
-    { id: 2, firstname: "Jana", surname: "Nováková", nationalId: "987654321" },
-    { id: 3, firstname: "Klára", surname: "Novotná", nationalId: "456789123" },
-  ]
+const students = [
+  { id: 1, firstname: "Petr", surname: "Novák", nationalId: "123456789" },
+  { id: 2, firstname: "Jana", surname: "Nováková", nationalId: "987654321" },
+  { id: 3, firstname: "Klára", surname: "Novotná", nationalId: "456789123" },
+]
 
+router.get("/list", (req, res) => {
   res.json(students)
 })
 
