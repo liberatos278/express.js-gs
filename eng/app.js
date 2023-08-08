6973845970eae05aa767db00dbe20912298dd80a
Liberatos <ja@nectu.email>
2023-08-08 20:57:23 +0200

    controllers

--- eng/app.js
+++ eng/app.js
@@ -3,10 +3,10 @@ const app = express()
 
 require("dotenv").config()
 
-app.use("/classroom", require("./controller/classroom.controller"))
-app.use("/student", require("./controller/student.controller"))
-app.use("/grade", require("./controller/grade.controller"))
-app.use("/subject", require("./controller/subject.controller"))
+app.use("/classroom", require("./controllers/classroom.controller"))
+app.use("/student", require("./controllers/student.controller"))
+app.use("/grade", require("./controllers/grade.controller"))
+app.use("/subject", require("./controllers/subject.controller"))
 
 app.listen(process.env.PORT, () =>
   console.log(`App running on port ${process.env.PORT}`)
