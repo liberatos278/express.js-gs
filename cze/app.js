a7170c4bff7b4723178cf7b93c0885179166c697
Liberatos <adam@svoboda.info>
2023-08-10 22:07:52 +0200

    init

--- cze/app.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const app = express()
-
-require("dotenv").config()
-
-app.listen(process.env.PORT, () =>
-  console.log(`Aplikace běží na portu ${process.env.PORT}`)
-)
