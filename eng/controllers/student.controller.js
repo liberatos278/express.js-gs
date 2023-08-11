1a755ea3430fb494def573fad473263143b05d5c
Liberatos <adam@svoboda.info>
2023-08-11 18:23:30 +0200

    fix: dotenv & unused vars

--- eng/controllers/student.controller.js
+++ eng/controllers/student.controller.js
@@ -4,12 +4,6 @@ const router = express.Router()
 const getStudent = require("../services/student/get.service")
 const listStudents = require("../services/student/list.service")
 
-const students = [
-  { id: 1, firstname: "Petr", surname: "Novák", nationalId: "123456789" },
-  { id: 2, firstname: "Jana", surname: "Nováková", nationalId: "987654321" },
-  { id: 3, firstname: "Klára", surname: "Novotná", nationalId: "456789123" },
-]
-
 router.get("/list", async (req, res) => {
   await listStudents(req, res)
 })
