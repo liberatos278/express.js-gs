0c46071fa38fe35036200f28bed1493461799b96
Liberatos <adam@svoboda.info>
2023-08-11 18:19:47 +0200

    unused vars

--- cze/controllers/grade.controller.js
+++ cze/controllers/grade.controller.js
@@ -4,8 +4,6 @@ const router = express.Router()
 const listGrades = require("../services/grade/list.service")
 const getGrade = require("../services/grade/get.service")
 
-const grades = []
-
 router.get("/list", async (req, res) => {
   await listGrades(req, res)
 })
--- cze/controllers/subject.controller.js
+++ cze/controllers/subject.controller.js
@@ -4,8 +4,6 @@ const router = express.Router()
 const getSubject = require("../services/subject/get.service")
 const listSubjects = require("../services/subject/list.service")
 
-const subjects = []
-
 router.get("/list", async (req, res) => {
   await listSubjects(req, res)
 })
