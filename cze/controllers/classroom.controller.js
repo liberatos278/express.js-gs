4031852a3421c73d8628820b4d0086656f3d5cf3
Liberatos <ja@nectu.email>
2023-10-12 18:23:40 +0200

    a

--- cze/controllers/classroom.controller.js
+++ cze/controllers/classroom.controller.js
@@ -6,6 +6,7 @@ const getClassroom = require("../services/classroom/get.service")
 const createClassroom = require("../services/classroom/create.service")
 const updateClassroom = require("../services/classroom/update.service")
 const deleteClassroom = require("../services/classroom/delete.service")
+const loadClassrooms = require("../services/classroom/load.service")
 
 router.get("/list", async (req, res) => {
   await listClassrooms(req, res)
@@ -13,6 +14,10 @@ router.get("/list", async (req, res) => {
 
 router.get("/get", async (req, res) => {
   await getClassroom(req, res)
+})
+
+router.get("/load", async (req, res) => {
+  await loadClassrooms(req, res)
 })
 
 router.post("/create", async (req, res) => {
---
+++ cze/services/classroom/load.service.js
@@ -0,0 +1,63 @@
+const path = require("path")
+const ClassroomDao = require("../../dao/classroom.dao")
+const StudentDao = require("../../dao/student.dao")
+const GradeDao = require("../../dao/grade.dao")
+const SubjectDao = require("../../dao/subject.dao")
+
+const classroomDao = new ClassroomDao(
+  path.join(__dirname, "..", "..", "data", "classrooms.json")
+)
+
+const studentDao = new StudentDao(
+  path.join(__dirname, "..", "..", "data", "students.json")
+)
+
+const gradeDao = new GradeDao(
+  path.join(__dirname, "..", "..", "data", "grades.json")
+)
+
+const subjectDao = new SubjectDao(
+  path.join(__dirname, "..", "..", "data", "subjects.json")
+)
+
+async function loadClassroom(req, res) {
+  const { id } = req.query
+  const classroom = await classroomDao.getClassroom(id)
+
+  if (!classroom)
+    return res.status(400).end(`Třída s identifikátorem ${id} neexistuje`)
+
+  const students = await studentDao.listStudents().then((students) => {
+    return students.filter((student) => student.classroomId === id)
+  })
+
+  const grades = await gradeDao.listGrades()
+  const subjects = await subjectDao.listSubjects()
+
+  students.forEach((student) => {
+    student.subjectList = []
+
+    subjects.forEach((subject) => {
+      let averageGrade = null
+      const studentSubjectGrades = grades.filter(
+        (grade) =>
+          grade.studentId === student.id && grade.subjectId === subject.id
+      )
+
+      let gradeSum = 0
+      let weightSum = 0
+
+      studentSubjectGrades.forEach((grade) => {
+        gradeSum += grade.grade * grade.weight
+        weightSum += grade.weight
+      })
+
+      if (gradeSum) averageGrade = gradeSum / weightSum
+      student.subjectList.push({ ...subject, averageGrade })
+    })
+  })
+
+  res.json({ ...classroom, studentList: students })
+}
+
+module.exports = loadClassroom
